c119b54796ca0e15cdd0a9c210431c1e5f7e257a
dawsonbooth <github@dawsonbooth.com>
2019-03-06 21:10:03 -0600

    Find packages automatically

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='vodscrepe',
-    version='0.0.4',
+    version='0.0.5',
     description='Vods.co Vod Scraper',
     author='Dawson Booth',
     author_email='pypi@dawsonbooth.com',
     url='https://github.com/dawsonbooth/vodscrepe',
-    packages=['vodscrepe'],
+    packages=find_packages(),
     license='MIT',
 )
