e64eb1a098a1be4cc7b8d888de62d3303cf390a9
dawsonbooth <github@dawsonbooth.com>
2020-04-03 15:03:38 -0500

    Parse all options

--- console.py
+++ console.py
@@ -9,7 +9,7 @@ def main():
 
     parser = argparse.ArgumentParser(
         description='Scrape the vods from http://vods.co/')
-    parser.add_argument('game', type=str,
+    parser.add_argument('game', type=str, default='melee',
                         help='The game whose vods will be scraped')
     parser.add_argument('-f', '--first', type=int, default=0,
                         help='First page of scraping')
@@ -19,9 +19,9 @@ def main():
                         help='Player 1')
     parser.add_argument('-p2', '--player2', type=str, default="",
                         help='Player 2')
-    parser.add_argument('-c1', '--char1', type=str, default="",
+    parser.add_argument('-c1', '--character1', type=str, default="",
                         help='Character 1')
-    parser.add_argument('-c2', '--char2', type=str, default="",
+    parser.add_argument('-c2', '--character2', type=str, default="",
                         help='Character 2')
     parser.add_argument('-e', '--event', type=str, default="",
                         help='Event')
@@ -33,15 +33,23 @@ def main():
                         help='Display scraping progress')
     parser.add_argument('-v', '--verbose', type=bool, default=False,
                         help='Display error statements')
+    parser.add_argument('-w', '--workers', type=int, default=10,
+                        help='Number of workers')
+    parser.add_argument('-pw', '--page-workers', type=int, default=1,
+                        help='Number of page workers')
 
     args = parser.parse_args()
 
-    s = Scraper(args.game)
+    s = Scraper(args.game, event=args.event, player1=args.player1, player2=args.player2, character1=args.character1,
+                character2=args.character2, caster1=args.caster1, caster2=args.caster2, num_workers=args.workers)
 
     pages = range(args.first, min(s.num_pages - 1, args.last))
     try:
-        for vod in s.scrape(pages, show_progress=args.progress, verbose=args.verbose):
+        for vod in s.scrape(pages, show_progress=args.progress, verbose=args.verbose, num_workers=args.page_workers):
             if vod is not None:
                 tqdm.write(formatted_title(vod))
     except KeyboardInterrupt:
         tqdm.write("Scraping terminated.")
+
+if __name__ == '__main__':
+    main()
