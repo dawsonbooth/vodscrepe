85599c57daef9178632db6581a6cdd31961871e7
dawsonbooth <github@dawsonbooth.com>
2020-04-03 15:02:54 -0500

    Add casters

--- vodscrepe/utils.py
+++ vodscrepe/utils.py
@@ -15,7 +15,7 @@ def formatted_title(vod) -> str:
     return title
 
 
-def build_url(video_game, event: str = None, player1: str = None, player2: str = None, character1: str = None, character2: str = None) -> str:
+def build_url(video_game, event: str = None, player1: str = None, player2: str = None, character1: str = None, character2: str = None, caster1: str = None, caster2: str = None) -> str:
     base_url = "https://vods.co/" + video_game
     if player1:
         base_url += "/player/" + player1
@@ -27,4 +27,8 @@ def build_url(video_game, event: str = None, player1: str = None, player2: str =
         base_url += "/character/" + character1
     if character2:
         base_url += "/character2/" + character2
+    if caster1:
+        base_url += "/caster/" + character1
+    if caster2:
+        base_url += "/caster2/" + character2
     return base_url
