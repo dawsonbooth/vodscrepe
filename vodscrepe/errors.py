c85e25b25265c82e36f57f78b5b9942f41626aae
dawsonbooth <github@dawsonbooth.com>
2020-04-02 22:29:10 -0500

    Add exception

--- vodscrepe/errors.py
+++ vodscrepe/errors.py
@@ -1,8 +1,13 @@
-class Error(Exception):
-    pass
 
 
-class InvalidVideoError(Error):
+
+class InvalidVideoException(Exception):
     def __init__(self, *args):
-        message = "Invalid Video: Vods.co vod '%s' links to invalid '%s' video: '%s'" % args
-        super(InvalidVideoError, self).__init__(message)
+        message = "Invalid Video: vods.co vod '%s' links to invalid video" % args
+        super(InvalidVideoException, self).__init__(message)
+
+
+class InvalidPageException(Exception):
+    def __init__(self, *args):
+        message = "Invalid Page: vods.co game '%s' does not have page '%i'" % args
+        super(InvalidPageException, self).__init__(message)
